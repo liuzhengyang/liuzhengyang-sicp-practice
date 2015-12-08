6ad6378fd2e6e910ca5ea03f964f1cd918a2f884
liuzhengyang <liuzhengyang@meituan.com>
2015-12-08 16:00:31 +0800

    sicp

--- ch2/2.42.rkt
+++ ch2/2.42.rkt
@@ -3,10 +3,10 @@
 (require "../commons.rkt")
 (define empty-board '())
 (define (safe-with-col k i positions)
-  (let ((vk (list-ref positions (- k 1)))
-        (vi (list-ref positions (- i 1))))
-    (not (or (= vk vi)
-         (= (abs (- i k)) (abs (- vk vi)))))))
+  (let ((valuek (list-ref positions (- k 1)))
+        (valuei (list-ref positions (- i 1))))
+    (nor (= valuek valuei)
+         (= (abs (- i k)) (abs (- valuek valuei)))))))
 
 (define (safe? k positions)
   (define (safe-iter i)
@@ -22,7 +22,7 @@
 (define (queens board-size)
   (define (queen-cols k)
     (if (= k 0) (list empty-board)
-        (filter
+       (filter
          (lambda (positions) (safe? k positions))
            (flatmap
             (lambda (rest-of-queens)
@@ -33,3 +33,22 @@
   (queen-cols board-size))
 
 (queens 8)
+( define  ( gen-list  start  end )
+   (  define  ( gen-iter  curr  result )
+         ( if  ( < curr  start )  result
+              ( gen-iter  ( - curr 1 )  (cons curr result ) ) ) )
+   ( gen-iter  end  nil ) )
+
+
+(gen-list 1 12)
+
+(define (my-map proc seq)
+  (if (null? seq) nil
+      (cons (proc (car seq)) ( my-map proc (cdr seq)))))
+
+
+(map (lambda (x) ( + x 1))
+     (map square
+          (gen-list 1 5)))
+
+
--- ch2/lisp-presentation.rkt
+++ ch2/lisp-presentation.rkt
@@ -10,3 +10,47 @@
 (define (square-of-square x)
     (square (square x)))
 (square-of-square 10)
+
+
+(define (abs x)
+  (if (> x 0)
+      x
+      (- x)))
+
+(lambda (x) (* x x))
+
+( define  ( sum-of-squares  x  y )
+  ( define  ( square  x )
+    ( *  x  x ) )
+  ( +  ( square  x)  ( square  y ) ) )
+
+
+
+
+
+   ( ( (lambda  ( x )  (lambda ( y ) ( + x  y ) )) 3 ) 4)
+
+(define nil '())
+(define (accumulate op init seq )
+  (if (null? seq ) init
+      ( op (car seq ) (accumulate op init (cdr seq)))))
+
+( define ( another-map proc seq )
+    ( accumulate
+        ( lambda  ( input  result )  ( cons ( proc input ) result ) ) 
+             nil  seq ) )
+
+
+( accumulate / 1 (list 1 2 3 4))
+( accumulate + 0 (list 1 2 3 4))
+
+(define (gen-list start end)
+  (if (> start end) nil
+      (cons start (gen-list ( + start 1 ) end))))
+( define  ( flatmap  proc  seq )
+    ( accumulate  append  nil 
+         ( map  proc  seq ))) 
+
+(map (lambda (x) (gen-list 1 x)) (list 1 2 3))
+(flatmap (lambda (x) (gen-list 1 x)) (list 1 2 3))
+
